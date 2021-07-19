e4eb17d671b42a03b8f300cfd21b2ad52ff9c068
Jnguyent <78618947+Jnguyent@users.noreply.github.com>
2021-07-19 00:49:39 -0500

    Added comments to exercise
    Specifically defined what a pangram is in the code solution

--- exercises/pangram/pangram.py
+++ exercises/pangram/pangram.py
@@ -1,7 +1,9 @@
+# Method to determine if a sentence is a pangram
+# a pangram is a sentence that uses each letter of the alphabet at LEAST once
 def is_pangram(sentence):
     sentence = sentence.lower()
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    
+   
     for char in alphabet:
         if char not in sentence:
             return False
